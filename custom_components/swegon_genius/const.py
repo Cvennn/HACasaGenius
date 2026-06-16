2c1bc3839137714b78a22852ba198ad9b7d719a4
Antti <antti.niiranen963@hotmail.fi>
2026-06-16 12:59:29 +0300

    integration setup

--- custom_components/swegon_genius/const.py
+++ custom_components/swegon_genius/const.py
@@ -1,8 +1,8 @@
-"""Constants for integration_blueprint."""
+"""Constants for swegon_genius."""
 
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-DOMAIN = "integration_blueprint"
+DOMAIN = "swegon_genius"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
